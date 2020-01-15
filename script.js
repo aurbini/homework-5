f373ab1e8493d83f5dad7e4fcd3a4647fafb98b4
Alex Urbini <aurbini92@gmail.com>
2020-01-15 12:58:23 -0500

    added color

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // console.log(moment().format());
 $(document).ready(function(){
+  var m = moment()
+  var currentDate = m.format("dddd MMM Mo YYYY");
   var wrapper = $('#wrapper'); 
+  var date = $('#date').text(currentDate);
+
+  var savedEvent; 
+  var savedEventTime; 
+  // console.log(currentDate); 
+
+   var wrapper = $('#wrapper'); 
 
   var eventsArray = []; 
   var eventsObj = {
@@ -8,32 +17,65 @@ $(document).ready(function(){
     event : ''
   }
 
-function renderEvents (){
-  wrapper.empty();
 
-  for(var i = 0; i < 9; i++){
-    var timedEvents = eventsArray[i];
-    //console.log(timedEvents); 
-    var container = $('<div>').addClass('container');
-    var form = $('<form>').addClass('form'); 
-    var time = $('<p>').addClass('time');
-    var input = $('<input>').addClass('events'); 
-    var saveButton = $('<button>').addClass('save'); 
-    saveButton.attr('type', 'submit'); 
-    time.text(i +9); 
-    if(timedEvents !== undefined){
-      console.log('event there')
-      console.log(timedEvents.event); 
-      input.val(timedEvents.event);} 
-    saveButton.text("save")
-   form.append(time).append(input).append(saveButton); 
-   container.append(form); 
-   wrapper.append(container); 
+  var now = moment(); 
+  var currentHour = now.format("HH");
+  function timeComparision(){ 
+  //console.log(currentHour); 
+  var toStringIndex; 
+    for(var i = 9; i < 15; i++){  
+      toStringIndex = i.toString(); 
+      //console.log(toStringIndex); 
+      if(parseInt(currentHour) === i){
+      $(`#${toStringIndex}`).css('background-color', 'green')
+      }else if(parseInt(currentHour) > i){
+        $(`#${toStringIndex}`).css('background-color', 'grey')
+
+      }
+     }
   }
-}
+
+  function renderEvents (savedEventTime,savedEvent){
+    wrapper.empty();
+
+    // console.log(eventsArray); 
+
+    for(var i = 0; i < 9; i++){
+      var index = i+9; 
+      var timedEvents = eventsArray;
+      var container = $('<div>').addClass('container');
+      var div = $('<div>').addClass('form'); 
+      var militaryTime = $('<p>').addClass('time');
+      var input = $('<input>').addClass('events'); 
+      var saveButton = $('<button>').addClass('save'); 
+      // saveButton.attr('type', 'submit'); 
+      militaryTime.text(i +9); 
+      //militaryTime.attr('id', i +9);
+      input.attr('id', i +9);
+      //console.log(input.attr('id'));
+     
+      //console.log('event there')
+        //console.log(timedEvents.event); 
+        input.val(timedEvents.event); 
+      saveButton.text("save")
+      div.append(militaryTime).append(input).append(saveButton); 
+      container.append(div); 
+      wrapper.append(container);
+      for(var j = 0; j < eventsArray.length; j++){
+          if(eventsArray[j].time === input.attr('id')){
+            $(`#${index}`).val(eventsArray[j].event);
+            //console.log($(`#${index}`))
+
+          }
+      } 
+     }
+    //console.log($("#9").text())
+
+    timeComparision(); 
+  }
 
   function storeEvents(savedEvents){
-  window.localStorage.setItem("savedEventsArray", JSON.stringify(eventsArray)); 
+    window.localStorage.setItem("savedEventsArray", JSON.stringify(eventsArray)); 
   }
 
   function init(){
@@ -46,19 +88,14 @@ function renderEvents (){
   }
   init(); 
 
-
   $('.save').click(function(event){
-    event.preventDefault(); 
-    var savedEvent = $(this).siblings('input').val().trim();
-    var savedEventTime = $(this).siblings('p').text();
+    savedEvent = $(this).siblings('input').val().trim();
+    savedEventTime = $(this).siblings('p').text();
     eventsObj.time = savedEventTime;
     eventsObj.event = savedEvent;
     eventsArray.push(eventsObj); 
-    //console.log(eventsArray); 
+    //console.log(eventsArray);
     storeEvents(); 
     renderEvents(); 
-  })
-  var currentTime = moment(); 
-  console.log("current time " + moment(currentTime).format("hh:mm"));
-  
+  });
 })
