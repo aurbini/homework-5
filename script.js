d072ccb0f068a93cbbe1f42b5e1f9abec0c12965
Alex Urbini <aurbini92@gmail.com>
2020-01-15 13:04:43 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -4,42 +4,32 @@ $(document).ready(function(){
   var currentDate = m.format("dddd MMM Mo YYYY");
   var wrapper = $('#wrapper'); 
   var date = $('#date').text(currentDate);
-
   var savedEvent; 
   var savedEventTime; 
-  // console.log(currentDate); 
-
-   var wrapper = $('#wrapper'); 
+  var wrapper = $('#wrapper'); 
 
   var eventsArray = []; 
   var eventsObj = {
     time : '' , 
     event : ''
   }
-
-
+//Add color functionality 
   var now = moment(); 
   var currentHour = now.format("HH");
   function timeComparision(){ 
-  //console.log(currentHour); 
   var toStringIndex; 
     for(var i = 9; i < 15; i++){  
       toStringIndex = i.toString(); 
-      //console.log(toStringIndex); 
       if(parseInt(currentHour) === i){
       $(`#${toStringIndex}`).css('background-color', 'green')
       }else if(parseInt(currentHour) > i){
         $(`#${toStringIndex}`).css('background-color', 'grey')
-
       }
      }
   }
-
+//Render the display 
   function renderEvents (savedEventTime,savedEvent){
     wrapper.empty();
-
-    // console.log(eventsArray); 
-
     for(var i = 0; i < 9; i++){
       var index = i+9; 
       var timedEvents = eventsArray;
@@ -48,15 +38,9 @@ $(document).ready(function(){
       var militaryTime = $('<p>').addClass('time');
       var input = $('<input>').addClass('events'); 
       var saveButton = $('<button>').addClass('save'); 
-      // saveButton.attr('type', 'submit'); 
       militaryTime.text(i +9); 
-      //militaryTime.attr('id', i +9);
       input.attr('id', i +9);
-      //console.log(input.attr('id'));
-     
-      //console.log('event there')
-        //console.log(timedEvents.event); 
-        input.val(timedEvents.event); 
+      input.val(timedEvents.event); 
       saveButton.text("save")
       div.append(militaryTime).append(input).append(saveButton); 
       container.append(div); 
@@ -64,37 +48,34 @@ $(document).ready(function(){
       for(var j = 0; j < eventsArray.length; j++){
           if(eventsArray[j].time === input.attr('id')){
             $(`#${index}`).val(eventsArray[j].event);
-            //console.log($(`#${index}`))
-
           }
       } 
      }
-    //console.log($("#9").text())
-
+    //Call the time comparision once the display is up 
     timeComparision(); 
   }
-
+//Save the data to local storage 
   function storeEvents(savedEvents){
     window.localStorage.setItem("savedEventsArray", JSON.stringify(eventsArray)); 
   }
-
+//initiate the calender and get the the data that is stored in LS if there is any 
   function init(){
     var savedEvents = JSON.parse(localStorage.getItem("savedEventsArray"));
     if(savedEvents !== null){
       eventsArray = savedEvents; 
     }
-    //console.log(eventsArray[0]); 
     renderEvents(); 
   }
   init(); 
-
+//add the data in the input field when clicked to the array 
+//call store function to store updated data
+//call render function to display updated data
   $('.save').click(function(event){
     savedEvent = $(this).siblings('input').val().trim();
     savedEventTime = $(this).siblings('p').text();
     eventsObj.time = savedEventTime;
     eventsObj.event = savedEvent;
     eventsArray.push(eventsObj); 
-    //console.log(eventsArray);
     storeEvents(); 
     renderEvents(); 
   });
